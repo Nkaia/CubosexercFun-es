1e045723cea332faf7c760fea4e75330621153c0
Daiane Santos <daiane.ssantos01@gmail.com>
2022-01-14 23:01:18 -0300

    funções dart3

---
+++ main.dart
@@ -0,0 +1,12 @@
+//resposta exer 1 funç 14/01
+
+void main() {
+  num valor1 = 5.3;
+  num valor2 = 4.25;
+
+  soma(valor1, valor2);
+}
+
+num? soma(num valor1, num valor2) {
+  print(valor1 + valor2);
+}
